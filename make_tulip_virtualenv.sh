cc23fc2108f31860499febcbf96558c5bce0acbd
Ioannis Filippidis <jfilippidis@gmail.com>
2014-09-05 20:55:46 -0700

    add tulip virtualenv setup, including SIP, PyQt

---
+++ make_tulip_virtualenv.sh
@@ -0,0 +1,32 @@
+envname=tulipdev
+
+if 0; then
+mkvirtualenv $envname
+pip install networkx
+pip install pyparsing
+pip install http://pydot.googlecode.com/files/pydot-1.0.28.tar.gz
+pip install ply
+pip install -q numpy
+pip install -q scipy
+pip install -q matplotlib
+
+pip install -q ipython
+pip install spyder
+fi
+export CVXOPT_BLAS_LIB="satlas,tatlas"
+export CVXOPT_BLAS_LIB_DIR="/opt/local/lib"
+export CVXOPT_LAPACK_LIB="lapack"
+
+# to verify that you are using the correct
+# "lib" and "include" directories, do:
+#
+#    ls /your/candidate/dir/ | grep glpk
+#
+# and you should find "glpk.h" and the
+# the glpk libraries (otherwise paths are wrong).
+export CVXOPT_BUILD_GLPK=1
+export CVXOPT_GLPK_LIB_DIR="/opt/local/lib"
+export CVXOPT_GLPK_INC_DIR="/opt/local/include"
+
+pip install cvxopt
+
---
+++ qt_sip_4_install.sh
@@ -0,0 +1,52 @@
+venvname=clean_dev
+
+which bison
+which flex
+pip install mercurial
+
+# =================
+# install SIP4
+# =================
+
+# link found from:
+#    http://www.riverbankcomputing.com/software/sip/download
+hg clone hg clone http://www.riverbankcomputing.com/hg/sip
+
+# installation instructions here:
+#    http://pyqt.sourceforge.net/Docs/sip4/installation.html
+cd sip
+python build.py prepare
+python configure.py --incdir ~/.virtualenvs/$venvname/include/python2.7/
+time make
+make install
+
+# ================
+# install PyQt4
+# ================
+# download link found here:
+#    http://www.riverbankcomputing.com/software/pyqt/download
+select=mac # or x11
+curl -LO http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.1/PyQt-$select-gpl-4.11.1.tar.gz
+tar vzxf PyQt-*
+
+# http://pyqt.sourceforge.net/Docs/PyQt4/installation.html
+cd PyQt-*
+
+# alternative:
+# curl -LO http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.1/PyQt-mac-gpl-4.11.1.tar.gz
+#
+python configure-ng.py
+time make
+make install
+
+# ===================
+# install spyder & dependencies
+# ===================
+pip install sphinx
+pip install rope
+pip install pyflakes
+pip install pylint
+pip install psutil
+pip install pyzmq
+pip install -I --allow-external spyder --egg spyder
+
